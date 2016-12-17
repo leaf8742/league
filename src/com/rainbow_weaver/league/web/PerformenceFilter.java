592978ca87918c06a278e55b364078ed8cef4932
LEAF.S <leaf_8742@163.com>
2016-12-17 21:32:05 +0800

    过滤器(xml)

---
+++ src/com/rainbow_weaver/league/web/PerformenceFilter.java
@@ -0,0 +1,37 @@
+package com.rainbow_weaver.league.web;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class PerformenceFilter
+ */
+public class PerformenceFilter implements Filter {
+    public PerformenceFilter() {
+        // TODO Auto-generated constructor stub
+    }
+    
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		long time = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		String uri = req.getRequestURI();
+		System.out.println("uri:" + uri + ":" + (System.currentTimeMillis() - time) + "ms");
+	}
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+}
